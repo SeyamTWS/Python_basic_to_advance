ea864cfb73be0ba390bafff4a058b1fb1e5785d5
SeyamTWS <mrshowiberahman@gmail.com>
2025-08-14 20:06:14 +0200

    Basic-Print

---
+++ basic_python/input.py
@@ -0,0 +1,5 @@
+# input
+name=input('what is your name? ')
+print('hello', name)
+uni=input('what is your uni name? ')
+print("your uni name is",uni)
---
+++ basic_python/variable.py
@@ -0,0 +1,8 @@
+seyam=10
+print("The value of seyam is:", seyam)
+# here seyam is a variable and 10 is the value assigned to it
+
+sorg="hello"
+
+print("The value of string is:",sorg)    
+# here"" is for a string and sorg is a variable that holds the string "hello"
